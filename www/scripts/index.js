d7848946e7003d86e705d03d4e8e383bc7d6af96
zAkazDrums A <serieszaka@gmail.com>
2019-12-03 18:16:48 +0100

    prueba

---
+++ www/scripts/index.js
@@ -0,0 +1,9 @@
+var sonido = '/musica/el-exorsista.mp3';
+
+var audio ='<embed id="musica" src="'+sonido+'" autoplay="false" type="audio/mpeg" enablejavascript="true"></embed>';
+document.write(audio);
+
+function reproducirmusica() {
+var tocando = document.getElementById('musica');
+tocando.Play();
+}
