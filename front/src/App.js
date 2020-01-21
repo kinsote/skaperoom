bddc2fe753ea6f778b5c19d1bafe7fe5f54035c7
zAkazDrums A <serieszaka@gmail.com>
2020-01-21 21:17:15 +0100

    zakaFront

---
+++ front/src/App.js
@@ -0,0 +1,23 @@
+import Nv2 from './nv2/components/Nv2';
+import React from 'react';
+
+
+function  App() {
+  return(
+    <div className="App">
+      
+     
+     <Nv2/>
+      
+
+     
+     
+
+    </div>
+  )
+}
+
+
+
+
+export default App; 
