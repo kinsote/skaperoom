1bdb6a408ec53cf0250981e20c77ad6b286ef7e4
zAkazDrums A <serieszaka@gmail.com>
2020-01-30 18:31:18 +0100

    x

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,12 @@
 import Nv2 from './nv2/components/Nv2';
 import React from 'react';
-import Quiz from './quiz/Index';
-
-
-
+import Quiz from './quiz/index'
 function  App() {
   return(
     <div className="App">
       
      
-     <Quiz/>
+     <Nv2/>
       
 
      
